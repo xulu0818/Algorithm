43c8e53b141bdf78b538adc05594ccbc0c784574
xulu0818 <xulu0818.email@gmail.com>
2020-05-05 15:29:51 -0400

    add listnode class to support reverse linked list

--- src/foundation/Linkedlist.java
+++ src/foundation/Linkedlist.java
@@ -1,17 +1,35 @@
 package foundation;
 
+import foundation.utilities.ListNode;
+
 public class Linkedlist {
   public static void main(String[] args){
-
+    Linkedlist linkedlist = new Linkedlist();
+    //create linked list: 2 --> 1 --> 3 --> 5 --> null
+    ListNode a1 = new ListNode(2);
+    ListNode a2 = new ListNode(1);
+    ListNode a3 = new ListNode(3);
+    ListNode a4 = new ListNode(5);
+    a1.next = a2;
+    a2.next = a3;
+    a3.next = a4;
+    a4.next = null;
+    ListNode reverseResult = linkedlist.reverseLinkedList(a1);
+    //System.out.println(new Object(){}.getClass().getEnclosingMethod().getName());
+    linkedlist.printHelper(reverseResult);
+    ListNode reverseRecurResult = linkedlist.reverseLinkedListRecur(reverseResult);
+    linkedlist.printHelper(reverseRecurResult);
   }
+
+  //T: O(n); S: O(1)
   public ListNode reverseLinkedList(ListNode head){
     if (head == null || head.next == null){
       return head;
     }
     ListNode prev = null;
     ListNode cur = head;
-    while(cur != null){
-      ListNode next = head.next;
+    while (cur != null){
+      ListNode next = cur.next;
       cur.next = prev;
       prev = cur;
       cur = next;
@@ -27,5 +45,12 @@ public class Linkedlist {
     head.next.next = head;
     head.next = null;
     return newHead;
+  }
+  public void printHelper(ListNode head){
+    while (head != null){
+      System.out.print(head.val + " --> ");
+      head = head.next;
+    }
+    System.out.println("null");
   }
 }
