0ecc359fd1607db441f6b33f70258f3c86c7fbd1
xulu0818 <xulu0818.email@gmail.com>
2020-10-25 21:28:54 -0700

    update KSmallestInUnsortedArray

--- src/practice/KSmallestInUnsortedArray.java
+++ src/practice/KSmallestInUnsortedArray.java
@@ -1,12 +1,15 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class KSmallestInUnsortedArray {
     public static void main(String[] args){
         KSmallestInUnsortedArray kSmallestInUnsortedArray = new KSmallestInUnsortedArray();
-        int[] kSmallestArray = kSmallestInUnsortedArray.kSmallest(new int[]{3,1,5,2,4}, 1);
+        int[] kSmallestArray = kSmallestInUnsortedArray.kSmallest(new int[]{3,1,5,2,4}, 3);
+        System.out.println(Arrays.toString(kSmallestArray));
     }
     public int[] kSmallest(int[] array, int k) {
         if (array == null || array.length == 0 || k == 0){
@@ -14,7 +17,6 @@ public class KSmallestInUnsortedArray {
         }
         int[] result = new int[k];
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, Collections.reverseOrder());
-        maxHeap.offer(array[0]);
         for (int i = 0; i < array.length; i++){
             if (i < k){
                 maxHeap.offer(array[i]);
@@ -25,7 +27,6 @@ public class KSmallestInUnsortedArray {
                 maxHeap.poll();
                 maxHeap.offer(array[i]);
             }
-            System.out.println(result.toString());
         }
         for (int i = k - 1; i >= 0; i--){
             result[i] = maxHeap.poll();
